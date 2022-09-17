7a4441736c1e5b7762dee46ccea13197171cac57
Jakub Grajber <grajber.jakub@gmail.com>
2022-09-17 07:41:56 +0200

    Clear project structure

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM amazoncorretto:17.0.4-al2
-
-ADD target/spring-boot-ecommerce-0.0.1-SNAPSHOT.jar ecommerce-app.jar
-
-ENTRYPOINT ["java", "-jar", "ecommerce-app.jar"]
--- src/main/java/com/jgrajber/ecommerce/dao/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jgrajber.ecommerce.dao;
-
-import com.jgrajber.ecommerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
