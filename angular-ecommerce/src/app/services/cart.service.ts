5bd03b92cf07bcfbcfc674c002b8c567a51d53fa
Jakub Grajber <grajber.jakub@gmail.com>
2022-09-23 16:01:04 +0200

    Add cart details view and functionality

--- angular-ecommerce/src/app/services/cart.service.ts
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -48,4 +48,24 @@ export class CartService {
 
 
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if(theCartItem.quantity === 0) {
+      this.remove(theCartItem)
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+  remove(theCartItem: CartItem) {
+
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+    if(itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
+    
+  }
 }
