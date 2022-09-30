173eea90179baacb845676ccc4dafa4dafa32163
Jakub Grajber <grajber.jakub@gmail.com>
2022-09-30 15:55:25 +0200

    Add validation for customer form

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { FormService } from 'src/app/services/form.service';
+import { CustomValidators } from 'src/app/validators/custom-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -30,9 +31,9 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+      firstName: new FormControl('', [Validators.required, Validators.minLength(2), CustomValidators.notOnlyWhiteSpace]),
+        lastName:  new FormControl('', [Validators.required, Validators.minLength(2), CustomValidators.notOnlyWhiteSpace]),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -87,7 +88,16 @@ export class CheckoutComponent implements OnInit {
   onSubmit() {
     console.log("Handling the submit button")
     console.log(this.checkoutFormGroup.get('customer').value);
+
+    if(this.checkoutFormGroup.invalid) { 
+      this.checkoutFormGroup.markAllAsTouched();
+    }
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName');}
+  get lastName () { return this.checkoutFormGroup.get('customer.lastName');}
+  get email() { return this.checkoutFormGroup.get('customer.email');}
+
 
   copyShippingAddressToBillingAddress(event) {
 
