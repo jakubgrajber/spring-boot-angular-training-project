7c68f163c95d81e12e3bf0aa86b179ba1fab9e4e
Jakub Grajber <grajber.jakub@gmail.com>
2022-10-04 17:47:46 +0200

    Add new tables to the db schema

--- backend/db-init-scripts/01-init.sql
+++ backend/db-init-scripts/01-init.sql
@@ -1,16 +1,12 @@
 CREATE SEQUENCE product_category_seq;
-
-
 CREATE TABLE IF NOT EXISTS product_category (
-                                                            id BIGINT NOT NULL DEFAULT NEXTVAL ('product_category_seq'),
-                                                          category_name VARCHAR(255) NULL DEFAULT NULL,
-                                                          PRIMARY KEY (id))
-;
-
+                                        id BIGINT NOT NULL DEFAULT NEXTVAL ('product_category_seq'),
+                                        category_name VARCHAR(255) NULL DEFAULT NULL,
+                                        PRIMARY KEY (id));
 ALTER SEQUENCE product_category_seq RESTART WITH 1;
 
-CREATE SEQUENCE product_seq;
 
+CREATE SEQUENCE product_seq;
 CREATE TABLE IF NOT EXISTS product (
     id BIGINT NOT NULL DEFAULT NEXTVAL ('product_seq'),
     sku VARCHAR(255) DEFAULT NULL,
@@ -23,36 +19,94 @@ CREATE TABLE IF NOT EXISTS product (
     date_created TIMESTAMP(6) DEFAULT NULL,
     last_updated TIMESTAMP(6) DEFAULT NULL,
     category_id BIGINT NOT NULL,
-    PRIMARY KEY (id)
-    ,
+    PRIMARY KEY (id),
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES product_category (id)
-    )
-;
-
+    );
 ALTER SEQUENCE product_seq RESTART WITH 1;
-
 CREATE INDEX fk_category ON product (category_id);
+
 
 CREATE TABLE country (
                          id smallint check (id > 0) NOT NULL,
                          code varchar(2) DEFAULT NULL,
                          name varchar(255) DEFAULT NULL,
                          PRIMARY KEY (id)
-) ;
+);
+
 
 CREATE SEQUENCE state_seq;
-
 CREATE TABLE state (
                        id smallint check (id > 0) NOT NULL DEFAULT NEXTVAL ('state_seq'),
                        name varchar(255) DEFAULT NULL,
                        country_id smallint check (country_id > 0) NOT NULL,
                        PRIMARY KEY (id),
                        CONSTRAINT fk_country FOREIGN KEY (country_id) REFERENCES country (id)
-)  ;
-
+);
 ALTER SEQUENCE state_seq RESTART WITH 1;
-
 CREATE INDEX fk_country ON state (country_id);
+
+
+CREATE SEQUENCE address_seq;
+CREATE TABLE address (
+                         id bigint NOT NULL DEFAULT NEXTVAL ('address_seq'),
+                         city varchar(255) DEFAULT NULL,
+                         country varchar(255) DEFAULT NULL,
+                         state varchar(255) DEFAULT NULL,
+                         street varchar(255) DEFAULT NULL,
+                         zip_code varchar(255) DEFAULT NULL,
+                         PRIMARY KEY (id)
+)   ;
+ALTER SEQUENCE address_seq RESTART WITH 1;
+
+
+CREATE SEQUENCE customer_seq;
+CREATE TABLE customer (
+                          id bigint NOT NULL DEFAULT NEXTVAL ('customer_seq'),
+                          first_name varchar(255) DEFAULT NULL,
+                          last_name varchar(255) DEFAULT NULL,
+                          email varchar(255) DEFAULT NULL,
+                          PRIMARY KEY (id)
+)   ;
+ALTER SEQUENCE customer_seq RESTART WITH 1;
+
+
+CREATE SEQUENCE orders_seq;
+CREATE TABLE orders (
+                        id bigint NOT NULL DEFAULT NEXTVAL ('orders_seq'),
+                        order_tracking_number varchar(255) DEFAULT NULL,
+                        total_price decimal(19,2) DEFAULT NULL,
+                        total_quantity int DEFAULT NULL,
+                        billing_address_id bigint DEFAULT NULL,
+                        customer_id bigint DEFAULT NULL,
+                        shipping_address_id bigint DEFAULT NULL,
+                        status varchar(128) DEFAULT NULL,
+                        date_created timestamp(6) DEFAULT NULL,
+                        last_updated timestamp(6) DEFAULT NULL,
+                        PRIMARY KEY (id),
+                        CONSTRAINT UK_billing_address_id UNIQUE (billing_address_id),
+                        CONSTRAINT UK_shipping_address_id UNIQUE (shipping_address_id),
+                        CONSTRAINT FK_customer_id FOREIGN KEY (customer_id) REFERENCES customer (id),
+                        CONSTRAINT FK_billing_address_id FOREIGN KEY (billing_address_id) REFERENCES address (id),
+                        CONSTRAINT FK_shipping_address_id FOREIGN KEY (shipping_address_id) REFERENCES address (id)
+);
+ALTER SEQUENCE orders_seq RESTART WITH 1;
+CREATE INDEX K_customer_id ON orders (customer_id);
+
+
+CREATE SEQUENCE order_item_seq;
+CREATE TABLE order_item (
+                            id bigint NOT NULL DEFAULT NEXTVAL ('order_item_seq'),
+                            image_url varchar(255) DEFAULT NULL,
+                            quantity int DEFAULT NULL,
+                            unit_price decimal(19,2) DEFAULT NULL,
+                            order_id bigint DEFAULT NULL,
+                            product_id bigint DEFAULT NULL,
+                            PRIMARY KEY (id),
+                            CONSTRAINT FK_order_id FOREIGN KEY (order_id) REFERENCES orders (id),
+                            CONSTRAINT FK_product_id FOREIGN KEY (product_id) REFERENCES product (id)
+);
+ALTER SEQUENCE order_item_seq RESTART WITH 1;
+CREATE INDEX K_order_id ON order_item (order_id);
 
 -- -- -----------------------------------------------------
 -- Categories
