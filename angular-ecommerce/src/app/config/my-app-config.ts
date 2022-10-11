36ecca43eabf8797f8a562a73f75d6edd448114d
Jakub Grajber <grajber.jakub@gmail.com>
2022-10-11 18:02:48 +0200

    Secure frontend communication with https

--- angular-ecommerce/src/app/config/my-app-config.ts
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa6rr0pm8HP3jrmy5d7',
         issuer: 'https://dev-96977370.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
