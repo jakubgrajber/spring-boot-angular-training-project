f393df2a14b3ba7665aa6333d951f551e971e8fe
Jakub Grajber <grajber.jakub@gmail.com>
2022-10-13 19:25:23 +0200

    Add support for Stripe payment processing

--- backend/src/main/java/com/jgrajber/ecommerce/controller/CheckoutController.java
+++ backend/src/main/java/com/jgrajber/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.jgrajber.ecommerce.controller;
 
+import com.jgrajber.ecommerce.dto.PaymentInfo;
 import com.jgrajber.ecommerce.dto.Purchase;
 import com.jgrajber.ecommerce.dto.PurchaseResponse;
 import com.jgrajber.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +24,15 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/jgrajber/ecommerce/service/CheckoutServiceImpl.java
+++ backend/src/main/java/com/jgrajber/ecommerce/service/CheckoutServiceImpl.java
@@ -1,23 +1,35 @@
 package com.jgrajber.ecommerce.service;
 
 import com.jgrajber.ecommerce.dao.CustomerRepository;
+import com.jgrajber.ecommerce.dto.PaymentInfo;
 import com.jgrajber.ecommerce.dto.Purchase;
 import com.jgrajber.ecommerce.dto.PurchaseResponse;
 import com.jgrajber.ecommerce.entity.Customer;
 import com.jgrajber.ecommerce.entity.Order;
 import com.jgrajber.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
-@AllArgsConstructor
 public class CheckoutServiceImpl implements CheckoutService{
 
     private CustomerRepository customerRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository,
+                               @Value("${stripe.key.secret}") String secretKey) {
+
+        this.customerRepository = customerRepository;
+
+        Stripe.apiKey = secretKey;
+    }
+
 
     @Override
     @Transactional
@@ -44,6 +56,20 @@ public class CheckoutServiceImpl implements CheckoutService{
         customerRepository.save(customer);
 
         return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
